94f77303492206c34dc12ce88b7ef09476828c15
sushmitha.dosada <sushmitha.dosada@BDC7-L-01345MP.dir.svc.accenture.com>
2023-07-28 19:19:55 +0530

    commiting addition2

---
+++ src/defaultpg/Addition2.java
@@ -0,0 +1,32 @@
+package defaultpg;
+
+import java.util.Scanner;
+
+public class Addition2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x,y,z;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the num");
+		x=sc.nextInt();
+		System.out.println("enter another num");
+		y=sc.nextInt();
+		z=x+y;
+		System.out.println("sum is:" +z);
+		System.out.println("sushmitha");
+		System.out.println("this is addition2");
+		
+		int a=5,b=4;
+		if(a>b) {
+			System.out.println("execute"); }
+		else if(a<b) {
+			System.out.println(" not execute");	
+			}
+		else {
+			System.out.println("exec");
+		}
+		}
+	}
+
+
